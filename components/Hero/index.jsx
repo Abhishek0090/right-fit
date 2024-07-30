cada24fd392b41da8e0b65521d90a671b8b05992
Abhishek0090 <palabhishek411@gmail.com>
2024-07-30 17:24:22 +0530

    fixes

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -3,7 +3,9 @@ export default function Hero() {
     <main className="w-full mx-auto py-8 h-[80vh]">
       <div className="text-start p-48 mb-8">
         <h1 className="text-7xl font-extrabold text-white">Latest Styles</h1>
-        <p className="text-xl text-white font-bold">At Yesterday's Prices</p>
+        <p className="text-xl text-white font-bold">
+          At Yesterday&apos;s Prices
+        </p>
         <button className="mt-4 px-5 py-3 bg-primaryColor text-white font-bold text-xl rounded-xl">
           Browse All Styles
         </button>
