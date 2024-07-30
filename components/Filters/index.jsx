1a0f1ec6212daa4e9ae8f3e699efe4616aab167c
Abhishek0090 <palabhishek411@gmail.com>
2024-07-30 17:33:29 +0530

    fix the code

--- components/Filters/index.jsx
+++ components/Filters/index.jsx
@@ -1,19 +1,43 @@
 "use client";
 
+import { useState } from "react";
 import { filterTypes } from "@/constants";
 
 export default function Filters() {
-  const applyFilter = () => {};
+  const [selectedFilters, setSelectedFilters] = useState({});
+
+  const applyFilter = (filterCategory, filterValue) => {
+    setSelectedFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterCategory]: filterValue,
+    }));
+  };
+
   return (
     <div className="h-full px-10 py-10 flex flex-col gap-10">
-      <h2 className="font-bold text-xl">Filter</h2>
+      <h2 className="font-bold text-xl">Filters</h2>
 
       {filterTypes.map(({ title, types }, idx) => (
         <div className="flex flex-col gap-y-2 cursor-pointer" key={idx}>
-          <h3>{title}</h3>
-          <span className="font-bold">All</span>
-          {types.map((item, idx) => (
-            <span key={idx}>{item}</span>
+          <h3 className="font-bold">{title}</h3>
+          <span
+            className={`font-bold ${
+              !selectedFilters[title] ? "text-blue-600" : ""
+            }`}
+            onClick={() => applyFilter(title, null)}
+          >
+            All
+          </span>
+          {types.map((item) => (
+            <span
+              key={item}
+              className={`${
+                selectedFilters[title] === item ? "text-blue-600" : ""
+              }`}
+              onClick={() => applyFilter(title, item)}
+            >
+              {item}
+            </span>
           ))}
         </div>
       ))}
