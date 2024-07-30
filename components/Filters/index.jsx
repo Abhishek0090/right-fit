a9b7239f51f2d02505dbf2d8004847a35d86b9b1
Abhishek0090 <palabhishek411@gmail.com>
2024-07-30 17:02:24 +0530

    bug fixes

--- components/Filters/index.jsx
+++ components/Filters/index.jsx
@@ -8,8 +8,8 @@ export default function Filters() {
     <div className="h-full px-10 py-10 flex flex-col gap-10">
       <h2 className="font-bold text-xl">Filter</h2>
 
-      {filterTypes.map(({ title, types }) => (
-        <div className="flex flex-col gap-y-2 cursor-pointer">
+      {filterTypes.map(({ title, types }, idx) => (
+        <div className="flex flex-col gap-y-2 cursor-pointer" key={idx}>
           <h3>{title}</h3>
           <span className="font-bold">All</span>
           {types.map((item) => (
--- components/Skeleton/index.jsx
+++ components/Skeleton/index.jsx
@@ -1,5 +1,5 @@
 export default function Skeleton() {
-  return [...Array(6)].map(() => (
-    <div className="p-4 m-4 h-[500px] shimmer rounded-lg"></div>
+  return [...Array(6)].map((_, idx) => (
+    <div className="p-4 m-4 h-[500px] shimmer rounded-lg" key={idx}></div>
   ));
 }
