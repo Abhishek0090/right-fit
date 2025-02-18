039d81a75c922cc1268797bec1971dce10f71eaf
Abhishek pal <palabhishek411@gmail.com>
2025-02-18 22:42:30 +0530

    addedd adsense code

--- components/GoogleBanner/index.jsx
+++ components/GoogleBanner/index.jsx
@@ -3,12 +3,17 @@ import { useEffect } from "react";
 
 const GoogleBannerAd = () => {
   useEffect(() => {
+    console.log("AdSense Component Mounted");
     if (typeof window !== "undefined" && window.adsbygoogle) {
       try {
+        console.log("Pushing AdSense Ad...");
         (window.adsbygoogle = window.adsbygoogle || []).push({});
+        console.log("AdSense Ad Pushed Successfully");
       } catch (e) {
         console.error("AdSense Error:", e);
       }
+    } else {
+      console.log("AdSense Script Not Loaded Yet");
     }
   }, []);
 
