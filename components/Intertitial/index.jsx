fd751f096623c5005e26112a0133cc170c60d6d0
Abhishek pal <palabhishek411@gmail.com>
2025-02-20 00:46:57 +0530

    merged code

--- components/Intertitial/index.jsx
+++ components/Intertitial/index.jsx
@@ -21,15 +21,15 @@ const InterstitialAd = () => {
         className="adsbygoogle"
         style={{
           display: "block",
-          // textAlign: "center",
-          // width: "1280px",
-          // height: "90px",
+          textAlign: "center",
+          width: "1280px",
+          height: "90px",
         }}
         // data-ad-layout="in-article"
-        data-ad-layout-key="-fb+5w+4e-db+86"
-        data-ad-format="fluid"
+        // data-ad-layout-key="-fb+5w+4e-db+86"
         data-ad-client="ca-pub-2344013691030339"
-        data-ad-slot="3515773446"
+        data-ad-slot="6156279036"
+        data-ad-format="auto"
       ></ins>
       <script>{`(adsbygoogle = window.adsbygoogle || []).push({});`}</script>
     </div>
