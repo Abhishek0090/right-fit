2ceea08c66a82c73c5c2ba76ff080efeabcc5fb5
Abhishek pal <81857184+Abhishek0090@users.noreply.github.com>
2025-02-19 18:36:50 +0530

    Update index.jsx

--- components/Intertitial/index.jsx
+++ components/Intertitial/index.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const InterstitialAd = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      (adsbygoogle = window.adsbygoogle || []).push({
+      (window.adsbygoogle = window.adsbygoogle || []).push({
         google_ad_client: "ca-pub-2344013691030339",
         enable_page_level_ads: true,
       });
